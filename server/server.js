3fa41586e3dffee0fcd27a879e73a7fc7b531376
heliang <leued2015@outlook.com>
2017-11-30 14:22:19 +0800

    add a

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
+
+
 app.use('/', express.static('./src'));
 app.use('/ex', express.static('./examples'));
 
 app.listen(8008);
+
+console.log('http://localhost:8008/ex/')
--- src/a.js
+++
@@ -1,2 +0,0 @@
-test fork
-test after fork 
