ee7fe2a8adba5d4e318c46a98ef31029d55e77ef
heliang <leued2015@outlook.com>
2017-12-11 15:48:54 +0800

    add build

---
+++ build/webpack/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+
+        rv: './src/core/rv.js',
+        lib: ['vue'],
+    },
+    output: {
+        path: path.join(__dirname, '../../dist'),
+        filename: "[name].js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '../../dist'),
+        compress: true,
+        port: 8082
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "lib",
+            filename: "lib.js",
+            
+        }),
+        new HtmlWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader" 
+            }
+        ]
+    }
+};
