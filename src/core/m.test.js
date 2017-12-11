8b9d1ac8b3c1d10eabc199509ce8e60905c3bc08
heliang <leued2015@outlook.com>
2017-12-11 15:48:17 +0800

    add to base

--- src/core/m.test.js
+++
@@ -1,6 +0,0 @@
-
-
-
-export function test(){
-	alert('dddddddd')
-}
--- src/core/rv.js
+++ src/core/rv.js
@@ -1,23 +1,14 @@
+// var a1 = require('vue');
+// console.log(a1)
 
-var $$$ = {};
+ import Vue from  'vue';
 
-// import {test as a} from './m.test.js';
-import vue from 'vue';
-console.log(vue)
-// import {Vue as vue} from './vue.esm.js'
-// console.log(vue)
-
-// console.log(111)
-
-// $$$['test'] = a;
-// $$$['test2'] = b;
-
-// alert(1)
-// export default $$$;
-
-
-
-
-
+ document.body.innerHTML = '<div id="app">{{message}}</div>'
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
 
 
