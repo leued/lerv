6d109b3da3643d1deaa47f10c9e679a533747e94
hsinlee <jimlee1103@163.com>
2017-12-18 15:21:07 +0800

    update config

--- build/webpack/webpack.config.js
+++ build/webpack/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             { 
                 test: /\.js$/, 
                 exclude: /node_modules/, 
-                loader: "babel-loader" 
+                use: {loader: "babel-loader"}
             },
 			{
                 test: /\.(css|less)$/,  
